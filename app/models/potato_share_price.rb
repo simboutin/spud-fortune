9bbda53af661f738299121b2766541a0c6efdfdd
Simon Boutin <simboutin@gmail.com>
2023-12-17 22:11:52 +0100

    add scopes to filter potato share prices on a specific date and to order them by time

--- app/models/potato_share_price.rb
+++ app/models/potato_share_price.rb
@@ -1,4 +1,7 @@
 class PotatoSharePrice < ApplicationRecord
   validates :time, presence: true
   validates :price_in_cents, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :on_date,         ->(date) { where(time: date.all_day) }
+  scope :ordered_by_time, -> { order(time: :asc) }
 end
--- spec/models/potato_share_price_spec.rb
+++ spec/models/potato_share_price_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PotatoSharePrice, type: :model do
-  describe 'validations' do
-    let!(:potato_share_price1) { PotatoSharePrice.create!(time: DateTime.new(2023, 12, 16, 12, 0, 0), price_in_cents: 100) }
+  let!(:date)                { Date.new(2023, 12, 16) }
+  let!(:potato_share_price1) { PotatoSharePrice.create!(time: DateTime.new(2023, 12, 16, 15, 0, 0), price_in_cents: 100) }
+  let!(:potato_share_price2) { PotatoSharePrice.create!(time: DateTime.new(2023, 12, 16, 12, 0, 0), price_in_cents: 200) }
+  let!(:potato_share_price3) { PotatoSharePrice.create!(time: DateTime.new(2023, 12, 15, 10, 0, 0), price_in_cents: 300) }
 
+  describe 'validations' do
     it 'is valid with valid time and price_in_cents' do
       expect(potato_share_price1).to be_valid
     end
@@ -26,6 +29,24 @@ RSpec.describe PotatoSharePrice, type: :model do
     it 'is not valid if price_in_cents is not an integer' do
       potato_share_price1.price_in_cents = 10.0
       expect(potato_share_price1).not_to be_valid
+    end
+  end
+
+  describe 'scopes' do
+    describe '::on_date' do
+      it 'returns the potato share prices on the specified date' do
+        expect(PotatoSharePrice.on_date(date)).to eq([potato_share_price1, potato_share_price2])
+      end
+
+      it 'does not return the potato share prices on other dates' do
+        expect(PotatoSharePrice.on_date(date)).not_to include(potato_share_price3)
+      end
+    end
+
+    describe '::ordered_by_time' do
+      it 'returns the potato share prices ordered by time ascending' do
+        expect(PotatoSharePrice.ordered_by_time).to eq([potato_share_price3, potato_share_price2, potato_share_price1])
+      end
     end
   end
 end
