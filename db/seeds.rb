d2267179392a03de13971af65de8621983a52650
Simon Boutin <simboutin@gmail.com>
2023-12-17 22:10:28 +0100

    seeds with fake data over the last 7 days

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Destroying potato share prices'
+PotatoSharePrice.delete_all
+
+puts 'Create potato share prices'
+start_time        = DateTime.new(2023, 12, 11, 0, 0, 0)
+seconds_in_7_days = 7 * 24 * 60 * 60
+price             = 205_00
+records           = []
+
+puts 'Creating fake PotatoSharePrice instances over the last 7 days...'
+
+seconds_in_7_days.times do |i|
+  # Gradually increase the range of the random fluctuation over time
+  fluctuation = rand((-10 - (i / seconds_in_7_days))..(10 + (i / seconds_in_7_days)))
+  price += fluctuation
+
+  # Constrain price to a range of 20000 to 21000 cents
+  price = price.clamp(200_00, 210_00)
+
+  records << { time: start_time + i.seconds, price_in_cents: price }
+end
+
+PotatoSharePrice.insert_all(records)
+
+puts "Finished: #{PotatoSharePrice.count} potato share prices have been successfully created"
